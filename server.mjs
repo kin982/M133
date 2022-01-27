5c2142c3c0963f412e865ec9f0e6dd95fc69b947
Benjamin Schleiss <Benjamin_Schleiss@sluz.ch>
2022-01-27 14:24:23 +0100

    aufgabe 3

--- server.mjs
+++ server.mjs
@@ -1,10 +1,14 @@
 import { createServer } from "http";
 
-const server = createServer();
-server;
+const server = createServer((request, response) => {
+  response.writeHead(200, { "content-type": "text/html charset=utf8" });
+  response.end(`
+    <h1>Hello World!</h1>
+    `);
+});
 server.listen(8080, (test) => {
-    console.log(test);
-    console.log(
-        `Server is listening to http://localhost.${server.address().port}`
-    );
+  console.log(test);
+  console.log(
+    `Server is listening to http://localhost.${server.address().port}`
+  );
 });
