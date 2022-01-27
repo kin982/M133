0e6a4a06f82e57ae79fd7037a11d21246585d0e6
Benjamin Schleiss <Benjamin_Schleiss@sluz.ch>
2022-01-27 14:46:11 +0100

    lektion ende

--- server.mjs
+++ server.mjs
@@ -1,7 +1,8 @@
 import { createServer } from "http";
-
+import url from "url";
 const server = createServer((request, response) => {
   response.writeHead(200, { "content-type": "text/html charset=utf8" });
+  console.log(new URL(request.url, "http://localhost:8080"));
   response.end(`
     <h1>Hello World!</h1>
     `);
